42ec2588c87872b4c6ade583227bf309f1047fb9
Michael Kropat <mail@michael.kropat.name>
2014-10-04 14:02:56 +0000

    Split pages apart

--- __init__.py
+++ __init__.py
@@ -3,10 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
-def hello():
+@app.route('/')
+def home():
     return app.send_static_file('index.html')
     #return "Hello, I love Digital Ocean!"
+
+@app.route('/iphone6')
+def iphone6():
+    return app.send_static_file('iphone6.html')
 
 if __name__ == "__main__":
     app.run()
