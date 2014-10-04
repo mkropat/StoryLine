50ebd3fa0b4d24ca558d7603f0699d80cb25af53
Michael Kropat <mail@michael.kropat.name>
2014-10-04 13:32:25 +0000

    Get d3 example live

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+app.debug = True
+
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!"
+    return app.send_static_file('index.html')
+    #return "Hello, I love Digital Ocean!"
+
 if __name__ == "__main__":
     app.run()
